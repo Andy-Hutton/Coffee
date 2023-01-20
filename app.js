ae49d92d5e2d98429bc1c96c9e45a7185adbb146
Andy Hutton <shandy_hutton@hotmail.com>
2023-01-20 16:45:23 +0000

    added pop up box

---
+++ app.js
@@ -0,0 +1,33 @@
+//  function guessANumber ("How many shots of coffee are in a doppio?)
+//  {let answer;
+//      // while the answer is incorrect, keep asking
+//     while (answer != 3) {
+//       answer = prompt("Guess a number between 0-10");
+
+//       if (answer != 3) {
+//         alert("Nope! Try again");
+//       } else {
+//         alert("Nice work! My favourite number is 3!");
+//         document.write("You guessed my number!");
+//       }
+//     }
+//   }
+
+// let myName = prompt("What is your name?");
+// let question2;
+// let question = prompt("do you like coffee?");
+
+// if (question.toLowerCase() == "yes") {
+//   message = "your in the right place";
+//   alert(message);
+//   question2 = prompt("how many do you drink each day?");
+//   if (question2 <= 5) {
+//     message = "You healthy thing " + myName + "!";
+//   } else {
+//     message = myName + "have a coffee and give it a go!";
+//     alert(message);
+//   }
+// }
+
+// console.log(message);
+// document.write(message);
